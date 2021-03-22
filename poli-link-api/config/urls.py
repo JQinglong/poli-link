271ff4fb5407cdbc6350de104b2c9b6b5ab72381
KONNO kiyotaka <konnokiyotaka@gmail.com>
2021-03-22 22:40:51 +0900

    swaggerの設定をルート用URLConfに組み込む

--- poli-link-api/config/urls.py
+++ poli-link-api/config/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from polilink_api.urls import router as polilink_api_router
+from rest_framework_swagger.views import get_swagger_view
+
+schema_view = get_swagger_view(title='API Lists')
 
 urlpatterns = [
     path('', include('polilink_api.urls')),
     path('admin/', admin.site.urls),
     path('api/', include(polilink_api_router.urls)),
+    path('swagger/', schema_view),
 ]
