c2f4dbe0673bdee6fc991c272a4e7b403ca2ada7
KONNO kiyotaka <konnokiyotaka@gmail.com>
2021-03-22 21:42:42 +0900

    ViewSetの定義

--- poli-link-api/polilink_api/views.py
+++ poli-link-api/polilink_api/views.py
@@ -1,5 +1,14 @@
+import django_filters
+
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import viewsets, filters
+from .models import Memo
+from .serializer import MemoSerializer
 
 def index(request):
     return HttpResponse("Hello, world.")
+
+class MemoViewSet(viewsets.ModelViewSet):
+    queryset = Memo.objects.all()
+    serializer_class = MemoSerializer
