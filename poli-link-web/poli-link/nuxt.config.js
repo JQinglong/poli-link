a6b6b47cbe8de7e28e181f74ff1429e863d4bf49
KONNO kiyotaka <konnokiyotaka@gmail.com>
2021-03-29 20:08:25 +0900

    axios対応

--- poli-link-web/poli-link/nuxt.config.js
+++ poli-link-web/poli-link/nuxt.config.js
@@ -30,7 +30,8 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    '@/plugins/composition-api'
+    '@/plugins/composition-api',
+    '@/plugins/axios-accessor'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
