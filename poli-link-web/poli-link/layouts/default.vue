629a20669f47fb4377eb24dea032071e616f0806
KONNO kiyotaka <konnokiyotaka@gmail.com>
2021-03-31 21:21:58 +0900

    layoutをtypescriptに変更

--- poli-link-web/poli-link/layouts/default.vue
+++ poli-link-web/poli-link/layouts/default.vue
@@ -88,35 +88,58 @@
   </v-app>
 </template>
 
-<script>
-export default {
-  data () {
+<script lang="ts">
+import { defineComponent, ref } from "@vue/composition-api";
+
+type Item = {
+  icon: string
+  title: string
+  to: string
+}
+
+export default defineComponent({
+  setup() {
+    const clipped = ref(false)
+    const drawer = ref(false)
+    const fixed = ref(false)
+
+    const items = ref<Item[]>([
+      {
+        icon: 'mdi-apps',
+        title: 'Welcome',
+        to: '/'
+      },
+      {
+        icon: 'mdi-apps',
+        title: 'memo',
+        to: '/memo'
+      },
+      {
+        icon: 'mdi-apps',
+        title: 'todolist',
+        to: '/todolist'
+      },
+      {
+        icon: 'mdi-chart-bubble',
+        title: 'Inspire',
+        to: '/inspire'
+      }
+    ])
+    const miniVariant = ref(false)
+    const right = ref(false)
+    const rightDrawer = ref(false)
+    const title = ref('Vuetify.js')
+
     return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
-        },
-        {
-          icon: 'mdi-apps',
-          title: 'memo',
-          to: '/memo'
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
-        }
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
+      clipped,
+      drawer,
+      fixed,
+      items,
+      miniVariant,
+      right,
+      rightDrawer,
+      title
     }
   }
-}
+})
 </script>
