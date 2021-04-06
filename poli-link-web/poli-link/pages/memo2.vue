ab23bd795e6be39ab452056a23198910010710f2
KONNO kiyotaka <konnokiyotaka@gmail.com>
2021-04-07 07:25:30 +0900

    最低限の実装をTypescriptに変更

--- poli-link-web/poli-link/pages/memo2.vue
+++ poli-link-web/poli-link/pages/memo2.vue
@@ -1,34 +1,56 @@
 <template>
   <div class="container">
-    <p>{{ memos }}</p>
+      <input v-model="state.memo" placeholder="メモを入力してください">
+      <br>
+      <li v-for="(memo, index) in state.memos" :key="index">
+        {{ memo }}
+      </li>
   </div>
 </template>
 
-<script>
+<script lang="ts">
+import { defineComponent, reactive, onMounted } from "@nuxtjs/composition-api"
 import { RepositoryFactory } from "~/repositories/RepositoryFactory";
 const MemoRepository = RepositoryFactory.get('memo')
-
-export default {
-  name: "name",
-  // components: { }
-  data() {
-    return {
-      isLoading: false,
-      memos: [],
-    }
-  },
-  created () {
-    this.fetch()
-  },
-  methods: {
-    async fetch () {
-      this.isLoading = true
-      const { data } = await MemoRepository.get()
-      this.isLoading = false
-      this.memos = data;
-    }
-  }
+interface StateData {
+  id: number
+  title: string
+  memo: string
+  isLoading: boolean,
+  memos: []
 }
+
+export default defineComponent({
+  head: {
+    title: "Memo"
+  },
+  setup(_props, context) {
+    const state = reactive<StateData>({
+      id: 0,
+      title: 'title0',
+      memo: 'memo0',
+      isLoading: false,
+      memos: []
+    })
+
+    const fetch = async () => {
+      state.isLoading = true
+      console.log('state.isLoading', state.isLoading)
+      const { data } = await MemoRepository.get()
+      state.isLoading = false
+      console.log('state.isLoading', state.isLoading)
+      console.log('data', data)
+      state.memos = data
+    }
+
+    onMounted(fetch) 
+
+    return {state}
+
+  }
+})
+
+
 </script>
 
 <style>
