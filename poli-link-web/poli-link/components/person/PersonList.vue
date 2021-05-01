8860bbbf995a027f4384d18d9f0df2fcd5130334
KONNO kiyotaka <konnokiyotaka@gmail.com>
2021-05-01 11:36:51 +0900

    PersonList

--- poli-link-web/poli-link/components/person/PersonList.vue
+++ poli-link-web/poli-link/components/person/PersonList.vue
@@ -2,7 +2,7 @@
   <div>
     <v-card>
       あかさたな
-      <v-data-table :headers="headers" :items="state.people" item-key="name" group-by="group" class="elevation-1" dense>
+      <v-data-table :headers="headers" :items="state.people" item-key="id" group-by="group" class="elevation-1" dense>
         <template v-slot:[`item.actions`]="{ item }">
           <v-icon @click="$router.push(`/person/${item.id}/`)"> mdi-tooltip-account </v-icon>
         </template>
@@ -22,32 +22,28 @@ export default defineComponent({
     const headers = [
       { text: '氏名', value: 'name' },
       { text: 'かな', value: 'name_kana' },
-      { text: '頭文字', value: 'group' },
       { text: '', value: 'actions', sortable: false },
     ];
     const { state: personState, getPersonList } = usePerson();
 
-    const fetchData = async (offset = 0) => {
-      await getPersonList({ offset });
-      // console.log('personState.personList', personState.personList)
-    };
-
-    const { fetchState } = useFetch(() => fetchData());
-
-    // 受け取ったAPIの結果を加工
-    // anyを駆使しているが、本当は、画面表示用の型定義をしてコピーするなど
     const state = reactive({
       people: [] as any[]
     })
-    for(let personItem of personState.personList) {
-      let person:any
-      person = personItem
-      person.group = personItem.name_kana.slice(0, 1)
-      state.people = [...state.people, person]
-      console.log('people', state.people)
-    }
-    // people = ref<{}[]>(personState.personList);
-      console.log(state.people)
+
+    const fetchData = async (offset = 0) => {
+      await getPersonList({ offset });
+      // console.log('personState.personList', personState.personList)
+      // 受け取ったAPIの結果を加工
+      // anyを駆使しているが、本当は、画面表示用の型定義をしてコピーするなど
+      for(let personItem of personState.personList) {
+        let person:any
+        person = personItem
+        person.group = personItem.name_kana.slice(0, 1)
+        state.people = [...state.people, person]
+      }
+    };
+
+    const { fetchState } = useFetch(() => fetchData());
 
     return {
       headers,
