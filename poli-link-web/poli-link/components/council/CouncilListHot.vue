c5fda2a2bd983011a50bff1b7491d2f73e2c0dc3
KONNO kiyotaka <konnokiyotaka@gmail.com>
2021-05-03 20:38:37 +0900

    CouncilListHot

---
+++ poli-link-web/poli-link/components/council/CouncilListHot.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <v-card dense>
+      <v-data-table :items="councilList" :search="search" hide-default-footer dense>
+        <template v-slot:item="{ item }">
+          <council-list-item :council="item" />
+        </template>
+      </v-data-table>
+    </v-card>
+  </div>
+</template>
+
+<script lang="ts">
+import { ref, toRefs, useFetch, defineComponent } from '@nuxtjs/composition-api';
+import CouncilListItem from '~/components/council/CouncilListItem.vue';
+import { useMinistry, useCouncil } from '@/compositions';
+
+// type CounsilItem = {
+//   id: string
+//   title: string
+//   subtitle: string
+//   headline: string
+//   action: string
+// }
+export default defineComponent({
+  name: 'CouncilListHot',
+  setup() {
+    const search = ref<String>('');
+    const selected = ref<Number[]>([2]);
+
+    const { state: ministryState, getMinistryList } = useMinistry();
+
+    const { state: councilState, getCouncilList } = useCouncil();
+
+    const fetchData = async (offset = 0) => {
+      await getMinistryList({ offset });
+      await getCouncilList({ offset });
+      // console.log('ministryState', ministryState)
+    };
+
+    const { fetchState } = useFetch(() => fetchData());
+
+    return {
+      search,
+      selected,
+      ...toRefs(ministryState),
+      ...toRefs(councilState),
+      fetchState,
+    };
+  },
+});
+</script>
--- poli-link-web/poli-link/pages/index.vue
+++ poli-link-web/poli-link/pages/index.vue
@@ -1,99 +1,66 @@
 <template>
-    <v-card
-      color="indigo lighten-4"
-    >
+  <v-card color="indigo lighten-4">
+    <v-toolbar dark dense color="blue-grey darken-4">
+      今話題の会議体
+      <v-spacer></v-spacer>
 
-    <v-data-table
-      :items="councils"
-      :search="search"
-      hide-default-footer
-      dense
-    >
-      <template v-slot:top>
-        <v-toolbar
-          dark
+      <v-text-field
+        v-model="search"
+        clearable
+        flat
+        dense
+        solo-inverted
+        hide-details
+        prepend-inner-icon="mdi-magnify"
+        label="Search"
+      ></v-text-field>
+      <template v-if="$vuetify.breakpoint.mdAndUp">
+        <v-spacer></v-spacer>
+        <v-select
+          v-model="sortBy"
+          flat
           dense
-          color="blue-grey darken-4"
-        >
-          今話題の会議体
-          <v-spacer></v-spacer>
+          solo-inverted
+          hide-details
+          :items="keys"
+          prepend-inner-icon="mdi-magnify"
+          label="Sort by"
+        ></v-select>
+        <v-spacer></v-spacer>
+        <v-btn-toggle v-model="sortDesc" mandatory>
+          <v-btn depressed small color="teal" :value="false">
+            <v-icon>mdi-arrow-up</v-icon>
+          </v-btn>
+          <v-btn depressed small color="teal" :value="true">
+            <v-icon>mdi-arrow-down</v-icon>
+          </v-btn>
+        </v-btn-toggle>
+      </template>
+    </v-toolbar>
 
-          <v-text-field
-            v-model="search"
-            clearable
-            flat
-            dense
-            solo-inverted
-            hide-details
-            prepend-inner-icon="mdi-magnify"
-            label="Search"
-          ></v-text-field>
-          <template v-if="$vuetify.breakpoint.mdAndUp">
-            <v-spacer></v-spacer>
-            <v-select
-              v-model="sortBy"
-              flat
-              dense
-              solo-inverted
-              hide-details
-              :items="keys"
-              prepend-inner-icon="mdi-magnify"
-              label="Sort by"
-            ></v-select>
-            <v-spacer></v-spacer>
-            <v-btn-toggle
-              v-model="sortDesc"
-              mandatory
-            >
-              <v-btn
-                depressed
-                small
-                color="teal"
-                :value="false"
-              >
-                <v-icon>mdi-arrow-up</v-icon>
-              </v-btn>
-              <v-btn
-                depressed
-                small
-                color="teal"
-                :value="true"
-              >
-                <v-icon>mdi-arrow-down</v-icon>
-              </v-btn>
-            </v-btn-toggle>
-          </template>
-        </v-toolbar>
-      
-      </template>
-      <template v-slot:item="{ item }">
-          <council-list-item :council="item" />
-        
-      </template>
-    </v-data-table>
+    <council-list-hot />
   </v-card>
 </template>
 
 <script lang="ts">
-import {
-  ref,
-  defineComponent,
-} from '@nuxtjs/composition-api'
+import { ref, defineComponent } from '@nuxtjs/composition-api';
 import CouncilListItem from '~/components/council/CouncilListItem.vue';
 type Item = {
-  id: string
-  title: string
-  subtitle: string
-  headline: string
-  action: string
-}
+  id: string;
+  title: string;
+  subtitle: string;
+  headline: string;
+  action: string;
+};
 
 export default defineComponent({
   components: { CouncilListItem },
   setup() {
-    const search = ref<String>('')
-
-    const selected = ref<Number[]>([2])
+    const search = ref<String>('');
+    const selected = ref<Number[]>([2]);
+    const sortDesc = ref<Boolean>(false);
+    const sortBy = ref<String>('Name');
+    const keys = ref<String[]>(['Name']);
 
     const councils = ref<Item[]>([
       {
@@ -116,14 +83,17 @@ export default defineComponent({
         headline: 'Brunch this weekend?',
         subtitle: `I'll be in your neighborhood doing errands this weekend. Do you want to hang out?`,
         title: '新型コロナウイルス感染症対策分科会',
-      }
-    ])
+      },
+    ]);
 
     return {
       search,
       selected,
+      sortDesc,
+      keys,
+      sortBy,
       councils,
-    }
-  }
-})
+    };
+  },
+});
 </script>
