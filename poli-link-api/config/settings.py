f91e163418681ee594717b483fe2f654d2022db1
KONNO kiyotaka <konnokiyotaka@gmail.com>
2021-05-06 21:09:08 +0900

    DEL SECRET_KEY

--- poli-link-api/config/settings.py
+++ poli-link-api/config/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'v&_ron8ao@etp@j%9(t7oai)u0p#mhzl&o*%pyuk)(17wfur5g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -148,3 +147,8 @@ REST_FRAMEWORK = {
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
 )
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
