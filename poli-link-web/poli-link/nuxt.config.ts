a628e03ecaade8f0861463c53d2e58907f1ee8ff
KONNO kiyotaka <konnokiyotaka@gmail.com>
2021-05-08 13:18:50 +0900

    invest firebase

--- poli-link-web/poli-link/nuxt.config.ts
+++ poli-link-web/poli-link/nuxt.config.ts
@@ -15,6 +15,17 @@ const { FIREBASE_PROJECT_ID,
 
 const config: Configuration = {
 
+  env: { 
+    FIREBASE_PROJECT_ID,
+    FIREBASE_API_KEY,
+    FIREBASE_AUTH_DOMAIN,
+    FIREBASE_DATABASE_URL,
+    FIREBASE_STORAGE_BUCKET,
+    FIREBASE_MESSAGING_SENDER_ID,
+    FIREBASE_APP_ID,
+    FIREBASE_MEASUREMENT_ID
+    },
+
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
 
--- poli-link-web/poli-link/plugins/firebase.config.ts
+++ poli-link-web/poli-link/plugins/firebase.config.ts
@@ -13,7 +13,7 @@ const config = {
   // measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 }
 
-console.log('config.apiKey', config.apiKey)
+console.log('config.apiKey', config)
 
 const firebase = Firebase.apps.length? Firebase.app():Firebase.initializeApp(config);
 export const auth = firebase.auth();
