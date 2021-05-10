8cca754466cd175f51c67d7c11c30bbe1f6186f3
KONNO kiyotaka <konnokiyotaka@gmail.com>
2021-05-10 19:23:47 +0900

    whitenoise

--- poli-link-api/config/settings.py
+++ poli-link-api/config/settings.py
@@ -51,9 +51,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
