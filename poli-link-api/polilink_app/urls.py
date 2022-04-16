c86c071357178e0ac809444312400d736c01693e
KONNO kiyotaka <konnokiyotaka@gmail.com>
2022-04-16 13:22:51 +0900

    「教育データの利活用に関する有識者会議」版作成 issue #14

--- poli-link-api/polilink_app/urls.py
+++ poli-link-api/polilink_app/urls.py
@@ -6,4 +6,6 @@ app_name = 'polilink_app'
 urlpatterns = [
     path('', views.index, name='index'),
     path('council_news_admin', views.council_news_admin, name='council_news_admin'),
+    # path('council_meeting_admin', views.council_meeting_admin, name='council_meeting_admin'),
+    path('council_meeting_admin', views.SearchMeetingView.as_view(), name='council_meeting_admin'),
 ]
---
+++ poli-link-api/polilink_app/views/__init__.py
@@ -0,0 +1,3 @@
+from .index import index
+from .council_news_admin import council_news_admin
+from .council_meeting_admin import SearchMeetingView
